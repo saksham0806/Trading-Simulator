daabd48929467e24b0c7ea147bbf70b21ea13bd5
saksham0806 <33366274+saksham0806@users.noreply.github.com>
2025-02-27 21:38:01 +0530

    added api for price checking

--- server.js
+++ server.js
@@ -1,23 +1,29 @@
-const express = require('express')
-const path = require('path')
-const fs = require("fs");
+import express from "express";
+import path from "path";
+import fs from "fs";
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express()
 
-app.use(express.static(`${__dirname}/templates`));
+app.use(express.static(`${__dirname}/routes`));
+app.use(express.static(`${__dirname}/routes/script/`));
 const port = 3000
 
-console.log(fs.readdirSync(`${__dirname}/templates/css`));
-
 app.get('/', (req, res) => {
-  res.sendFile(`${__dirname}/templates/home.html`);
+  res.sendFile(`${__dirname}/routes/home.html`);
 })
 
 app.get('/signinup', (req, res) => {
-    res.sendFile(`${__dirname}/templates/signinup.html`);
+    res.sendFile(`${__dirname}/routes/signinup.html`);
 })
 
 app.get('/dashboard', (req, res) => {
-    res.sendFile(`${__dirname}/templates/dashboard.html`);
+    res.sendFile(`${__dirname}/routes/dashboard.html`);
+})
+app.get('/test', (req, res) => {
+    res.sendFile(`${__dirname}/routes/test.html`);
 })
 
 app.listen(port, () => {
