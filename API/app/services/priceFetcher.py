b05e85d90331e1ee16d987f8b05fbcfefb432d93
Saksham0806 <sakshamsingh0806@gmail.com>
2026-02-23 21:29:39 +0530

    updated to massive

--- API/app/services/priceFetcher.py
+++ API/app/services/priceFetcher.py
@@ -3,10 +3,10 @@ from dotenv import load_dotenv
 import requests
 
 load_dotenv()
-POLYGON_API_KEY = os.getenv("POLYGON_API_KEY")
+MASSIVE_API_KEY = os.getenv("MASSIVE_API_KEY")
 url = (
     f"https://api.massive.com/v2/aggs/ticker/NVDA/range/1/minute/2026-01-01/2026-01-02"
-    f"?adjusted=true&sort=asc&limit=50000&apiKey={POLYGON_API_KEY}"
+    f"?adjusted=true&sort=asc&limit=50000&apiKey={MASSIVE_API_KEY}"
 )
 
 res = requests.get(url)
